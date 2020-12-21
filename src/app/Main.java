8de5fe5e9c8faf216bcc1b3ab74aed7f11320259
Sajmir Doko <sajdoko@gmail.com>
2020-12-21 09:27:17 +0100

    login screen

--- src/app/Main.java
+++ src/app/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../view/login.fxml"));
         primaryStage.setTitle("Store Manager");
-        primaryStage.setScene(new Scene(root, 1200, 600));
+        primaryStage.setScene(new Scene(root, 1280, 800));
         primaryStage.show();
     }
 
